8162aa8f859b8d08ef5c933dcf7eaa0839113cd5
jasmyn13 <jasmyn13@gmail.com>
2022-08-29 10:44:03 -0400

    made AMI change

--- main.tf
+++ main.tf
@@ -1,11 +1,9 @@
 provider "aws" {
-   region     = "us-east-2"
-   
+   region     = "us-east-1"   
 }
 
-resource "aws_instance" "terraformgitssh" {
-
-    ami = "ami-05fa00d4c63e32376"  
+resource "aws_instance" "terraformssh" {
+    ami = ""
     instance_type = "t2.micro" 
     key_name= "aws_key"
     vpc_security_group_ids = [aws_security_group.main.id]
